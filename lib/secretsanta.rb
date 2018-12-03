46158a23b44d6ae2fb11a172ba4951e6e30f5fa3
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2018-12-02 20:11:06 -0500

    Work in progress

--- lib/secretsanta.rb
+++ lib/secretsanta.rb
@@ -10,12 +10,56 @@ module SecretSanta
   module_function
 
   def notify_participants!(options)
-    generate_mappings(options.participants)
+    client = Twilio::REST::Client.new(options.twilio_account_sid, options.twilio_auth_token)
+
+    generate_mappings(options.participants).each do |mapping|
+      client.messages.create(
+        body: "Happy holidays! You have been assigned to give a gift to #{mapping.recipient_name} this year.",
+        from: options.from_number,
+        to: mapping.sender_number
+      )
+    end
   end
 
-  private
+  def generate_mappings(participants_list)
+    list = []
+    participants = participants_list.each do |p|
+      p[:has_assignment] = false
+      p[:is_assigned] = false
+    end
 
-  def generate_mappings(participants_json)
-    participants_json
+    loop do
+      participants.each do |participant|
+        recipient = participants[rand 0..participants.length - 1]
+
+        unless participant[:disallow].includes?(recipient[:name]) ||
+               participant[:has_assignment] ||
+               recipient[:is_assigned]
+          list << { sender_number: participant[:number], recipient_name: recipient[:name] }
+
+          participant[:has_assignment] = true
+          recipient[:is_assigned] = true
+        end
+      end
+
+      break if participants.all? { |p| p[:has_assignment] && p[:is_assigned] }
+    end
+
+    list
+  end
+
+  def help_text
+    <<~HEREDOC
+      SecretSanta, version #{VERSION}
+
+      To run, provide command-line arguments or set corresponding environment variables:
+
+      - TWILIO_ACCOUNT_SID
+      - TWILIO_AUTH_TOKEN
+      - TWILIO_PHONE_NUMBER
+
+      Command-line arguments:
+
+    HEREDOC
   end
 end
--- secretsanta.gemspec
+++ secretsanta.gemspec
@@ -20,6 +20,7 @@ Gem::Specification.new do |spec|
   spec.executables   = ['secretsanta']
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'dotenv'
   spec.add_dependency 'twilio-ruby'
 
   spec.add_development_dependency 'bundler', '~> 1.17'
